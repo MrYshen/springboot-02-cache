76f544e33c909bdbc3d97387321da7eae1da2d10
Mr.Yshen <351171891@qq.com>
2020-01-08 16:51:44 +0800

    cacheTest01

--- src/main/java/edu/hust/cache/controller/EmployeeController.java
+++ src/main/java/edu/hust/cache/controller/EmployeeController.java
@@ -1,4 +1,19 @@
 package edu.hust.cache.controller;
 
+import edu.hust.cache.bean.Employee;
+import edu.hust.cache.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/emp/{id}")
+    public Employee getEmployee(@PathVariable("id") Integer id){
+        Employee employee = employeeService.getEmp(id);
+        return  employee;
+    }
 }
