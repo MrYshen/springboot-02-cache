0a2c0394db36217cc4c079444ce07c1808d4e92c
Mr.Yshen <351171891@qq.com>
2020-01-11 11:26:54 +0800

    2020-1-11 11:25:40 Redis cache

--- src/test/java/edu/hust/cache/Springboot01CacheApplicationTests.java
+++ src/test/java/edu/hust/cache/Springboot01CacheApplicationTests.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,6 +18,33 @@ class Springboot01CacheApplicationTests {
     @Autowired
     EmployeeMapper employeeMapper;
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;//操作k-v都是字符串的
+
+    @Autowired
+    RedisTemplate redisTemplate;//k-v都是对象的
+
+    /**
+     * String、List、Set、Hash、Zset(有序集合)
+     * stringRedisTemplate.opsForValue();操作字符串的
+     * stringRedisTemplate.opsForList();操作列表的
+     */
+    @Test
+    public void test01(){
+//        //给redis中保存数据
+//        stringRedisTemplate.opsForValue().append("msg", "hello");
+//        String s = stringRedisTemplate.opsForValue().get("msg");
+//        System.out.println(s);
+        stringRedisTemplate.opsForList().leftPush("mylist", "1");
+        stringRedisTemplate.opsForList().leftPush("mylist", "2");
+
+    }
+
+    @Test
+    public void test02(){
+        Employee employee = employeeMapper.getEmpById(1);
+        redisTemplate.opsForValue().set("emp-01", employee);
+    }
 
     @Test
     void contextLoads() {
